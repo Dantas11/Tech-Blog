e16fa7f918faddcf16432c80814a86635046ea49
Thiago Dantas <thiagoo.dantas11@gmail.com>
2023-09-08 20:04:02 +1000

    set up of mudules, sequelize, express, middleware and handlebars in server.js

---
+++ server.js
@@ -0,0 +1,48 @@
+// Import required modules and libraries
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+
+// Import and configure Sequelize and the session store
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Create an Express app and set the port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+// Configure the session object for the app
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+// Use the session middleware for the app
+app.use(session(sess));
+
+// Set up the app to use Handlebars.js as its template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Configure the app to use JSON and URL-encoded data and static files
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use the imported routes for the app
+app.use(routes);
+
+// Sync the database and start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
